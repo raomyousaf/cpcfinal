cc33763fe303d5662fb893fe3c9de950475a15a2
raomyousaf <57899440+raomyousaf@users.noreply.github.com>
2023-08-29 14:52:08 +0500

    Setup
    Basic of express

--- index.js
+++ index.js
@@ -2,14 +2,33 @@
 const express = require('express')
 const app = express()
 
+//import dotenv file
+require("dotenv").config();
 //Import the main Passport and Express-Session library
 const passport = require('passport')
 const session = require('express-session')
 
 //Import the secondary "Strategy" library
 const LocalStrategy = require('passport-local').Strategy
-
-
 // In this example we will use the "local" strategy
 
 
+const bodyParser = require("body-parser");
+const path = require("path");
+
+//// define middleware & public folder
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+
+app.listen(process.env.PORT);
