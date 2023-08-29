8d6e4d72767ea642e1e56391af7b4b72bd899199
raomyousaf <57899440+raomyousaf@users.noreply.github.com>
2023-08-29 23:26:31 +0500

    theme
    Its theme setting

--- index.js
+++ index.js
@@ -20,14 +20,16 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.use(express.json());
 
 
 
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+   res.render("./Dashboard");
   })
   
 
